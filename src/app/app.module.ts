7cccb2bcc9f1d04e997214cfc2fce618a3a98da1
TourBandLive <tourbandlive@gmail.com>
2024-11-18 19:31:29 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
